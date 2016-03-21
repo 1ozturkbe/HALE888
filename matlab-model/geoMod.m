b867d9e32a66d294e2985d9428639ddafd678b4a
1ozturkbe <1ozturkbe@gmail.com>
2016-03-21 12:28:30 -0400

    New framework to create geometries within geomod.

--- matlab-model/geoMod.m
+++ matlab-model/geoMod.m
@@ -8,14 +8,30 @@ fileID = fopen(strcat('avl_geometries/haleMod',suf,'.avl'),'w');
 fileRex = '[a-zA-Z]+[0-9a-zA-Z]*\.dat';
 
 %Geometry Modification parameters
-sweepMod = 1.5;
-dihedralMod = 1;
-%chordMod = [1.0 1 1 1 1 1 1 1 1 1 1 1 1 1 1];
-chordMod = [1.5 1.05 1.025 1.0 1 1 1 1 1 1 1 1 1 1 1];
-leMod = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
-twistMod = [4 3 2 1.5 1 0.5 0 0 0 0 0 -0.5 -1 -1.5 -2.75];
+%sweepMod = 1.5;
+%dihedralMod = 1;
+%chordMod = [1.5 1.05 1.025 1.0 1 1 1 1 1 1 1 1 1 1 1];
+%leMod = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+%twistMod = [4 3 2 1.5 1 0.5 0 0 0 0 0 -0.5 -1 -1.5 -2.75];
 %twistMod = zeros(1,15);
 %flapHingeMod = [0.9 0.7 0.7 0.7 0.7];
+
+%Initial inputs
+initRef = [15.91 0.755 21.06]; %Sref, Cref, Bref (ft)
+initWing = [0 0 0 1 0 0 0;
+    0 3 0 1 0 0 0;
+    0.0655 6 0 0.7378 0 0 0;
+    0.1202 8.5 0 0.5194 0 0 0;
+    0.142 9.5 0 0.432 0 0 0;
+    0.1645 10.53 0 0.342 0 0 0];
+%Xle Yle Zle Chord Ainc Nspanwise Sspace
+
+% Generate new planform
+chordMod = [1.0 1 1 1 1 1];
+twistMod = [4 3 2 1.5 1 0];
+leMod = [0 -0.2 -0.3 -0.2 -0.1 0];
+sweepMod = 1.5; dihedralMod = 1.1;
+bMod = 1.5;
 
 line = fgets(file);
 fprintf(fileID,'%s \n',line);
@@ -36,7 +52,7 @@ while ischar(line)
     %     end
     section = regexp(line,'SECTION','match');
     fprintf(fileID,'%s',line);
-    if ~isempty(section)
+    if ~isempty(section) && secID < 6
         secID = secID + 1;
         %Reading Xle,Yle,Zle,Chord,Ainc title
         line = fgets(file);
@@ -51,7 +67,7 @@ while ischar(line)
         Ainc = posData{1}(5);
         %Modifying Xle,Yle,Zle,Chord,Ainc values
         Xle = sweepMod*Xle+leMod(secID);
-        Yle = Yle;
+        Yle = Yle*bMod;
         Zle = dihedralMod*Zle;
         chord = chordMod(secID)*chord;
         Ainc = twistMod(secID) + Ainc;
@@ -61,6 +77,15 @@ while ischar(line)
             '   ',num2str(Zle),...
             '   ',num2str(chord),...
             '   ',num2str(Ainc)]);
+        fprintf(fileID,'%s \n',newLine);
+    elseif ~isempty(section)
+        secID = secID + 1;
+        %Reading Xle,Yle,Zle,Chord,Ainc title
+        line = fgets(file);
+        fprintf(fileID,'%s',line);
+        %Reading Xle,Yle,Zle,Chord,Ainc values
+        line = fgets(file);
+        newLine = line;
         fprintf(fileID,'%s \n',newLine);
     end
     airfoilfile = regexp(line,fileRex,'match');
