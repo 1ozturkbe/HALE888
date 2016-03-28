c08ae702d0375cc33e57c7e1eaa356e37a18e3dc
Johannes Norheim <johannes.j.norheim@gmail.com>
2016-03-28 12:25:48 -0400

    made avlRun a function for easier use

--- matlab-model/avlRun.m
+++ matlab-model/avlRun.m
@@ -1,12 +1,10 @@
-%% Create run file
-% TODO: test and convert to function
+function [CL, CD, alphas] = avlRun(runname, M, a)
 % Code readapted from http://www.uavs.us/2011/12/02/matlab-avl-control/
+% AVL located in avl_geometries/avl.exe, all airfoils in local path
+% runname = 'hale';
+% M = 0.1314;
+% a = 340.3;
 tic
-% AVL located in avl_geometries/avl.exe
-% .avl filepath relative to avl.exe
-runname = 'hale';
-M = 0.1314;
-a = 340.3;
 velocity = M*a;
 avlfilepath = ['../avl_template_geometries/' runname, '.avl'];
 runfilename = 'sweepalphas';
@@ -15,7 +13,6 @@ outfilename = 'angleofattack';
 outfilepath = ['avl_run_outputs/' runfilename]; 
 alphas = -3:15;
 
-%% Operate AVL
 % Overwrite input file
 fid = fopen(runfilepath, 'w');
 %Load the AVL definition of the aircraft
@@ -42,13 +39,13 @@ fprintf(fid, '\n');
 fprintf(fid, 'Quit\n'); 
 fclose(fid);
 
-% Run filename.run with AVL
+%% Run filename.run with AVL
 % preallocate filespace for the .st files, so that they can be overwritten
 arrayfun(@(alpha) createrunfiles(...
     sprintf('%s%d.st',outfilepath,alpha)), alphas);
 cd('airfoils_and_geometries');
 [status,result] = dos(['avl.exe < ../' runfilepath]);
-disp(result);
+%disp(result);
 cd('../');
 
 %% Reap the results
@@ -56,12 +53,12 @@ CL = [];
 CD = [];
 CM = [];
 xnp = [];
-% for alpha = alphas
-%     runinfo = getruninfo([outfile num2str(alpha) '.st']);
-%     CL = [CL runinfo.CLtot];
-%     CD = [CD runinfo.CDtot];
-%     CM = [CM runinfo.CMtot];
-%     xnp = [xnp runinfo.xnp];
-% end
-% LoD = CL./CD;
+for alpha = alphas
+    runinfo = getruninfo([outfilepath num2str(alpha) '.st']);
+    CL = [CL runinfo.CLtot];
+    CD = [CD runinfo.CDtot];
+    CM = [CM runinfo.CMtot];
+    xnp = [xnp runinfo.xnp];
+end
 toc
+end
