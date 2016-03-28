14a3190a7c80ef22cd990f0e88b83cdf0137773b
Johannes Norheim <johannes.j.norheim@gmail.com>
2016-03-27 21:08:04 -0400

    airfoil parsing working

--- matlab-model/volumeCalc.m
+++ matlab-model/volumeCalc.m
@@ -1,15 +1,39 @@
-function area = volumeCalc(airfoilpath, tolerablethickness)
+%function area = volumeCalc(airfoilpath, tolerablethickness)
     % tolerablethickness is in fractional percentage (0.05)
+    airfoilpath = 'orig0foilmod.dat';
+    tolerablethickness = 0.05;
+    regex = '([-+]?([0-9]*\.[0-9]+|[0-9]+)\s[-+]?([0-9]*\.[0-9]+|[0-9]+))';
     fid = fopen(airfoilpath);
-    xycell = textscan(fid, '%f%f', 'CollectOutput', 1);
+    tline = fgetl(fid);
+    matchinglines = '';
+    while ischar(tline)
+        if regexp(tline, regex)
+            matchinglines = [matchinglines tline '\n'];
+        end
+        tline = fgetl(fid);
+    end
     fclose(fid);
+    s = sprintf(matchinglines);
+    xycell = textscan(s, '%f %f \n', 'CollectOutput', 1);
     xyarray = xycell{1};
     len = length(xyarray);
-    [~, ~, idx2] = unique(xyarray(:,1));
-    top = xyarray(1:len/2, 2);
-    bottom = xyarray(len/2:end, 2);
-    
-    idx = find(xyarray == xyarray(1:len/2,1));
-    plot(xyarray(:,1),xyarray(:,2))
-    area = polyarea(xyarray(:,1),xyarray(:,2));
-end
+    top = xyarray(1:len/2, :);
+    bottom = xyarray(len/2+1:end, :);
+    [~, order] = sort(top(:,1), 'ascend');
+    sortedtop = top(order,:);
+    [~, order] = sort(bottom(:,1), 'ascend');
+    sortedbottom = bottom(order,:);
+    botyinterp = interp1(bottom(:,1), bottom(:,2), top(:,1));
+    height = top(:,2)-botyinterp;
+    idx = find(height>=tolerablethickness);
+    filteredtop = sortedtop(idx,:);
+    filteredbottom = sortedbottom(idx,:);
+    [~, order] = sort(filteredbottom(:,1), 'descend');
+    filteredbottom = filteredbottom(order,:);
+    xyfiltered = [filteredtop;filteredbottom];
+    x = xyfiltered(:,1);
+    y = xyfiltered(:,2);
+    plot(x,y)
+    axis equal
+    area = polyarea(x,y)
+%end
