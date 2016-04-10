b06075eee18af196e002163a40ae7494d979205d
Johannes Norheim <johannes.j.norheim@gmail.com>
2016-04-10 16:28:07 -0400

    cleaning up wing evaluation code

--- matlab-model/evalWing.m
+++ matlab-model/evalWing.m
@@ -1,48 +1,19 @@
-function [L, LoD, W_wing, fuelVolume, delta_tip] = evalWing(arr) 
+function [L, LoD, W_wing, fuelVolume, delta_tip] = evalWing(arr)
+% usage:
 % vect = [1 1 0.5 0 0 0 0 0 0]
 % [L, LoD, W_wing, fuelVolume, delta_tip] = evalWing(vect)
-global initWing initRef 
-global cri bi
 global rho V a
+global newInd
 
-% Importing globals
-%SrefInit = initRef(1); crefInit = initRef(2); brefInit = initRef(3);
-crInit = initWing(1,4); bInit = initWing(6,2);
-newWing = initWing;
-newRef = initRef;
+[wingDescription, wingRef] = geoDescription(arr);
 
-% "Decipher" the vector
-% Format of arr is as follows:
-% arr = [bScaling crScaling lam a1 a2 a3 a4 a5 a6]
-bInd = 1; crInd = 2; lamInd = 3; aInd = 4:9;
-bi = bInit * arr(bInd);     % Note that bi is the half-span
-cri = crInit * arr(crInd);  % root chord
-lami = arr(lamInd);         % taper ratio
-alphai = arr(aInd);         % angle mod for each section
-
-% Note that the center 3 ft of the wing will be designer to be a straight
-% section. 
-bCent = 3; % [ft]
-
-% Rewriting wing reference array
-newRef(1) = 2 * (bCent*cri + (1+lami)/2*cri*(bi-bCent)); % reference area
-newRef(3) = 2 * bi * arr(bInd);                          % reference span
-newRef(2) = newRef(1) / newRef(3);                       %reference chord
-
-% Rewriting wing geometry array
-newWing(3:6,2) = newWing(3:6,2) * arr(bInd); % scaling y position of le
-ctip = cri*lami;
-chordArr = [chordArr; linspace(cri,cri*lami,5)'];
-newWing(:,1) = chordArr/4; % Wing c/4 aligned along wing.
-newWing(:,4) = chordArr;
-newWing(:,5) = newWing(:,5) + arr(aInd)';
-%Evaluating performance of wings (structural, fuel capacity)
+% Evaluating performance of wings (structural, fuel capacity)
 [W_wing, delta_tip] = structRun();
-fuelVolume = fuelVol(newWing);
+fuelVolume = fuelVol(wingDescription);
 %Putting the newWing into AVL format using geoMod
 geoMod;
 %total_weight = 71.41*.454*9.81+W_wing; %N
-[L, LoD] = LoDeval(newInd, newRef(1), rho, V, a);
+[L, LoD] = LoDeval(newInd, wingRef(1), rho, V, a);
 
 
 
---
+++ matlab-model/geoDescription.m
@@ -0,0 +1,37 @@
+function [wingDescription, wingRef] = geoDescription(arr) 
+% vect = [1 1 0.5 0 0 0 0 0 0]
+global initWing
+global cri bi
+
+% Importing globals
+%SrefInit = initRef(1); crefInit = initRef(2); brefInit = initRef(3);
+crInit = initWing(1,4); bInit = initWing(6,2);
+wingDescription = initWing;
+wingRef = zeros(1,3);
+
+% "Decipher" the vector
+% Format of arr is as follows:
+% arr = [bScaling crScaling lam a1 a2 a3 a4 a5 a6]
+bInd = 1; crInd = 2; lamInd = 3; aInd = 4:9;
+bi = bInit * arr(bInd);     % Note that bi is the half-span
+cri = crInit * arr(crInd);  % root chord
+lami = arr(lamInd);         % taper ratio
+alphai = arr(aInd);         % angle mod for each section
+
+% Note that the center 3 ft of the wing will be designer to be a straight
+% section. 
+bCent = 3; % [ft]
+
+% Rewriting wing reference array
+wingRef(1) = 2 * (bCent*cri + (1+lami)/2*cri*(bi-bCent)); % reference area
+wingRef(3) = 2 * bi * arr(bInd);                          % reference span
+wingRef(2) = wingRef(1) / wingRef(3);                     % reference chord
+
+% Rewriting wing geometry matrix
+% scaling y position of leading edge
+wingDescription(3:6,2) = wingDescription(3:6,2) * arr(bInd); 
+% ctip = cri*lami;
+chordArr = [cri; linspace(cri,cri*lami,5)'];
+wingDescription(:,1) = chordArr/4; % Wing c/4 aligned along wing.
+wingDescription(:,4) = chordArr;
+wingDescription(:,5) = wingDescription(:,5) + alphai';
