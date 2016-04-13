ff36163cd33824c1dc8edc67d893ef64e9d790ef
1ozturkbe <1ozturkbe@gmail.com>
2016-04-12 20:53:04 -0400

    Added improved perturbation function. Also added planform fo initial design.

--- matlab-model/SAWingPerturb.m
+++ matlab-model/SAWingPerturb.m
@@ -2,14 +2,25 @@ function pertWing = SAWingPerturb(mod)
 % Perturbs the wing mod function for simulated annealing
 global bInd crInd lamInd 
 pertWing = [];
+% for i = 1:length(mod)
+%     if i == bInd
+%         pertWing(i) = .8 + rand()*0.4;
+%     elseif i == crInd
+%         pertWing(i) = .75 + rand()*.5;
+%     elseif i == lamInd
+%         pertWing(i) = rand();
+%     else    
+%         pertWing(i) = -2 + rand()*4;
+%     end
+% end
 for i = 1:length(mod)
     if i == bInd
-        pertWing(i) = .8 + rand()*0.4;
+        pertWing(i) = mod(i)*(0.9 + rand()*0.2);
     elseif i == crInd
-        pertWing(i) = .75 + rand()*.5;
+        pertWing(i) = mod(i)*(.9 + rand()*.2);
     elseif i == lamInd
-        pertWing(i) = rand();
+        pertWing(i) = min(mod(i)*(.8 + rand()*.4),1);
     else    
-        pertWing(i) = -2 + rand()*4;
+        pertWing(i) = mod(i) -1 + rand()*2;
     end
 end
