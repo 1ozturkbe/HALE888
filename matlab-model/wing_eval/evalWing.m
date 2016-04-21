f31c8400f7571f3f977edfe655b9426aa3d12754
Johannes Norheim <johannes.j.norheim@gmail.com>
2016-04-20 23:14:24 -0400

    evalWing now has automatic cashe

--- matlab-model/wing_eval/evalWing.m
+++ matlab-model/wing_eval/evalWing.m
@@ -1,23 +1,6 @@
 function [L, LoD, W_wing, fuelVolume, delta_tip, extrainfo] = evalWing(arr, newInd)
-% usage:
-% vect = [1 1 0.5 0 0 0 0 0 0]
-% [L, LoD, W_wing, fuelVolume, delta_tip] = evalWing(vect)
-global rho V a
-global newWing newRef
-global xarr
-xarr = [xarr arr];
-disp(arr)
-[wingDescription, wingRef, cri] = geoDescription(arr);
-
-% Evaluating performance of wings (structural, fuel capacity)
-[W_wing, delta_tip] = structRun(wingRef, cri);
-fuelVolume = fuelVol(wingDescription);
-%Putting the newWing into AVL format using geoMod
-newWing = wingDescription;
-newRef = wingRef;
-geoMod(newInd);
-%total_weight = 71.41*.454*9.81+W_wing; %N
-[L, LoD, extrainfo] = LoDeval(newInd, wingRef(1), rho, V, a);
+[L, LoD, W_wing, fuelVolume, delta_tip, extrainfo] = ...
+    evalWingStoreResults(arr, newInd);
 
 
 
