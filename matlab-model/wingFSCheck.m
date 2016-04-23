33f6363925c9c12aa7276b58828d46640b8e2e21
1ozturkbe <1ozturkbe@gmail.com>
2016-04-23 19:53:15 -0400

    got wingFSCheck.m working. Now it can scour the wings and evaluate which ones satisfy the tip deflection and fuel volume constraints

--- matlab-model/wingFSCheck.m
+++ matlab-model/wingFSCheck.m
@@ -1,18 +1,21 @@
 addpath('wing_eval/savedruns')
 %import('storedWingEvaluations.mat');
-global delta0b_max fuelVolReq
+global delta0b_max fuelVolReq initRef
 
 wings = savedEvaluations.values;
 keys = savedEvaluations.keys;
 
 validWings = [];
-cFuel = false;
-cDelta = false;
-c
 
-for i = 1:length(keys)
+for i = 1:length(wings)
    wingRes = wings{i};
+   L = wingRes.L;
+   W_wing = wingRes.W_wing; 
+   W_tot = W_wing + 71.41*.454*9.81;
    fuelVolume = wingRes.fuelVolume;
-   delta_tip = wingRes.delta_tip;
-   L = wingRes.L;   
+   b = wingRes.arr(1)*initRef(3);
+   delta0b = wingRes.delta_tip/b;
+   if L > 0.95*W_tot && delta0b < delta0b_max && fuelVolume > fuelVolReq
+       validWings = [validWings wingRes];
+   end
 end
