6283ef16c582579f9bcfe4e741d9009ef652fb91
Johannes Norheim <johannes.j.norheim@gmail.com>
2016-04-25 23:20:42 -0400

    Fixed bugs with global variable

--- matlab-model/wing_eval/LoDeval.m
+++ matlab-model/wing_eval/LoDeval.m
@@ -2,11 +2,9 @@ function [Lift, LoD, extrainfo] = LoDeval(newInd, Sref, rho, V, a)
     suf = num2str(newInd);
     runname = ['haleMod', suf];
     % allow for the option to not store every single plane evaluation
-    global save_geometry_file
-    if exists('save_geometry_file')
-        if ~save_geometry_file
-            runname = 'haleMod';
-        end
+    global not_save_geometry_file
+    if not_save_geometry_file
+        runname = 'haleMod';
     end
     disp(runname)
     [CL, CD, ~, extrainfo] = avlRun(runname, V/a);
