01877dc885933da8fdd6f9eb7a9d16285be927ba
Johannes Norheim <johannes.j.norheim@gmail.com>
2016-04-25 23:13:47 -0400

    added option to not save all geometry files

--- matlab-model/wing_eval/LoDeval.m
+++ matlab-model/wing_eval/LoDeval.m
@@ -1,6 +1,13 @@
 function [Lift, LoD, extrainfo] = LoDeval(newInd, Sref, rho, V, a)
     suf = num2str(newInd);
     runname = ['haleMod', suf];
+    % allow for the option to not store every single plane evaluation
+    global save_geometry_file
+    if exists('save_geometry_file')
+        if ~save_geometry_file
+            runname = 'haleMod';
+        end
+    end
     disp(runname)
     [CL, CD, ~, extrainfo] = avlRun(runname, V/a);
     Lift = CL*0.5*rho*V^2*Sref*.3048^2;
