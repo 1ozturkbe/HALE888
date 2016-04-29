d477fbe5f0f0cc74e88ffa3beb51d8bf5c038eb8
1ozturkbe <1ozturkbe@gmail.com>
2016-04-28 23:06:22 -0400

    Added getLoD and getW_wing to calculate gradients of objectives

---
+++ matlab-model/getLoD.m
@@ -0,0 +1,2 @@
+function LoD = getLoD(arr,ind)
+[L, LoD, W_wing, fuelVolume, delta_tip, extrainfo] = evalWing(arr, ind)
---
+++ matlab-model/getW_wing.m
@@ -0,0 +1,2 @@
+function W_wingout = getW_wing(arr,ind)
+[L, LoD, W_wing, fuelVolume, delta_tip, extrainfo] = evalWing(arr, newInd)
